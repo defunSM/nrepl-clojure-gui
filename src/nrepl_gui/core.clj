6b383c6aae3cdb2ac9d14ee266f55db064bf8e9d
defunSM <sxh8083@g.rit.edu>
2016-07-13 20:29:08 -0400

    adding

--- src/nrepl_gui/core.clj
+++ src/nrepl_gui/core.clj
@@ -48,7 +48,7 @@
       (run-command))))
 
 (def display-area
-  (text :multi-line? true :text "Clojure 1.8.0 nRepl GUI (Salman Hossain)                                                                                               \n\n\n\n\n\n\n\n\n\n\n" :foreground "white" :background "black" :wrap-lines? true :columns 40))
+  (text :multi-line? true :editable? false :text "Clojure 1.8.0 nRepl GUI (Salman Hossain)                                                                                               \n\n\n\n\n\n\n\n\n\n\n" :foreground "white" :background "black" :wrap-lines? true :columns 40))
 
 (def direct-input
   (text :multi-line? false :text "" :foreground "yellow" :listen [:key-typed keypress]))
@@ -65,8 +65,47 @@
               :south (clojure-input)
               :vgap 5 :hgap 5 :border 5))
 
+(defn laf-selector []
+  (horizontal-panel
+    :items [
+            (combobox
+              :model    (vals (SubstanceLookAndFeel/getAllSkins))
+              :renderer (fn [this {:keys [value]}]
+                          (text! this (.getClassName value)))
+              :listen   [:selection (fn [e]
+                                      ; Invoke later because CB doens't like changing L&F while
+                                      ; it's doing stuff.
+                                      (invoke-later
+                                        (-> e
+                                          selection
+                                          .getClassName
+SubstanceLookAndFeel/setSkin)))])]))
+
+(defn handler [event]
+  (let [e (.getActionCommand event)]
+    (if (= e "Exit")
+      (System/exit 0))
+    (if (= e "Stop nREPL server")
+      (stop-server server))
+    (if (= e "Select Theme")
+      (-> (frame :title "Themes" :id 3 :content (laf-selector) :on-close :hide :height 600 :width 300) pack! show!))))
+
+(def exit-program (menu-item :text "Exit"
+                              :tip "Closes the entire program."
+                              :listen [:action handler]))
+
+(def stopping-server (menu-item :text "Stop nREPL server"
+                             :tip "Stops the current nREPL server."
+                             :listen [:action handler]))
+
+(def select-theme (menu-item :text "Select Theme"
+                             :tip "Allows you to change the current theme."
+                             :listen [:action handler]))
+
 (def f (frame :title "nRepl GUI"
               :id 1
+              :menubar (menubar :items
+                                [(menu :text "File" :items [stopping-server select-theme exit-program])])
               :width 640
               :height 480
               :on-close :exit
